ba124d56e7d2ae215d17984dd887878541f8b588
zl1fly <robert+github@kneedrag.org>
2014-06-09 21:05:18 +1200

    a

--- serial_mon.py
+++ serial_mon.py
@@ -18,8 +18,8 @@ temp = 0
 #function to split the string and work out which value to update
 def process_string(string_to_process):
   words = string.split(string_to_process)
-  print(words[0]+"")
-  print(words[1]+"")
+  print(words[0]+"\n")
+  print(words[1]+"\n")
   return
 
 def write_to_db():
