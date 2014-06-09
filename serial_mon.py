63f89bf628dae1e1dc84098808c062049ca33f64
zl1fly <robert+github@kneedrag.org>
2014-06-09 22:50:31 +1200

    DB stuff

--- serial_mon.py
+++ serial_mon.py
@@ -4,6 +4,10 @@ import serial
 #Import the string handling lib's
 import string
 
+#Import the mysql lib's
+import MySQLdb
+
+
 # Port and speed settings
 port = "/dev/ttyACM0"
 
@@ -12,6 +16,11 @@ complete_string = ""
 humidity = 0
 temp = 0
 dbupdate = 0
+
+#database values
+database = "yourdb"
+user = "youruser"
+db_pass = "yourpassword"
 
 # Set up the serial port 9600 (8N1=Default)
 ser = serial.Serial(port,9600)
@@ -46,6 +55,10 @@ def write_to_db():
     global humidity
     global temp
     
+    db = MySQLdb.connect("localhost", db_user, db_pass, database)
+    cursor = db.cursor()
+    
+
     # If there has been an update update the DB
     if dbupdate == 1:
         if (temp != 0) and (humidity != 0):
@@ -53,6 +66,17 @@ def write_to_db():
             print("Temperature = "+str(temp)+"C")
             #reset the DB flag to 0
             dbupdate = 0
+            #build the sql string
+            sql = "INSERT INTO cabin_values \
+            (temp, humidity, timestamp) VALUES \
+            ('%f', '%f', now()) " % \
+            (temp, humidity)
+            try:
+                cursor.excute(sql)
+                db.commit()
+            except:
+                db.rollback()
+    db.close()            
     return
 
 # Main program starts here.
