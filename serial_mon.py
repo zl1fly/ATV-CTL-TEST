76cdccccaa408572f85b5b9b2b5a48571c4192a1
zl1fly <robert+github@kneedrag.org>
2014-06-09 20:08:15 +1200

    Added some comments
    Cleaned up the code.

--- serial_mon.py
+++ serial_mon.py
@@ -1,5 +1,7 @@
 #Imports the serial lib's
 import serial
+
+#Import the string handling lib's
 import string
 
 # Port and speed settings
@@ -7,13 +9,15 @@ port = "/dev/ttyACM0"
 
 # Set up the serial port 9600 (8N1=Default)
 ser = serial.Serial(port,9600)
+
+#Initialise variables.
 complete_string = ""
 
+#function to split the string
 def process_string(string_to_process):
-  print(string_to_process)
   words = string.split(string_to_process)
-  #print(words[0]+"\n")
-  #print(words[1]+"\n")
+  print(words[0]+"\n")
+  print(words[1]+"\n")
   return
 
 
@@ -22,8 +26,11 @@ while 1:
     # Read from the serial port
     value = ser.read()
 
+    #Check to see if the character read from the serial port is a newline
+    #If it is a line feed then call process the string function
     if value == "\n":
       process_string(complete_string)
       complete_string = ""
+    #Otherwise concatenate the string
     else:
       complete_string = complete_string + value
