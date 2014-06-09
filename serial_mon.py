89e3c2ccd42f4e244c01296833035c6178d52408
zl1fly <robert+github@kneedrag.org>
2014-06-09 21:19:04 +1200

    grrr

--- serial_mon.py
+++ serial_mon.py
@@ -18,9 +18,10 @@ temp = 0
 #function to split the string and work out which value to update
 def process_string(string_to_process):
   words = string.split(string_to_process)
-  if words[0] != ("a:" or "b:"):
-    print("no joy")
-  else:
+  if words[0] == "a:":
+    print(words[0]+"\n")
+    print(words[1]+"\n")
+  if words[0] == "b:":
     print(words[0]+"\n")
     print(words[1]+"\n")
   return
